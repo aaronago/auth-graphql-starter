e305c32e2dbb293a514dbf82acd1863431044673
Aaron Gottlieb <atticus19@gmail.com>
2017-11-07 08:17:25 -0700

    adds user types

--- server/server.js
+++ server/server.js
@@ -12,13 +12,14 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://grauth:ql1234@ds149495.mlab.com:49495/grauthql';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
 // on success or failure
+
 mongoose.connect(MONGO_URI);
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
